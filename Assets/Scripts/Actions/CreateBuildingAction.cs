83c8ca4cbc5f689f3e293dd3740d9e4c5227f351
Vasileios Vrouliotis <vvrouliotis@gmail.com>
2018-12-06 20:04:23 +0200

    Command Base Ghost completed.

--- Assets/Scripts/Actions/CreateBuildingAction.cs
+++ Assets/Scripts/Actions/CreateBuildingAction.cs
@@ -3,10 +3,39 @@ using UnityEngine;
 
 namespace Actions
 {
-	public class CreateBuildingAction : ActionBehavior {
+	public class CreateBuildingAction : ActionBehavior
+	{
+
+		public GameObject GhostBuildingPrefab;
+
+		private GameObject active = null;
+		
 		public override Action GetClickAction()
 		{
-			return delegate { Debug.Log("Create Command Base Attempt"); };
+			return delegate
+			{
+				var go = GameObject.Instantiate(GhostBuildingPrefab);
+				go.AddComponent<FindBuildingSite>();
+				active = go;
+			};
+		}
+
+		void Update()
+		{
+			if (active == null)
+				return;
+
+			if (Input.GetKeyDown(KeyCode.Escape))
+			{
+				GameObject.Destroy(active);
+			}
+		}
+
+		private void OnDestroy()
+		{
+			if (active == null)
+				return;
+			Destroy(active);
 		}
 	}
 }
---
+++ Assets/Scripts/FindBuildingSite.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindBuildingSite : MonoBehaviour {
+	// Update is called once per frame
+	void Update ()
+	{
+		var tempTarget = RtsManager.Current.ScreenPointToMapPosition(Input.mousePosition);
+		if (tempTarget.HasValue == false)
+			return;
+		transform.position = tempTarget.Value;
+	}
+}
