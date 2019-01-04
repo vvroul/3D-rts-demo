c96fd43ef6c46e92a1462aba17ff5492c505078b
Vasileios Vrouliotis <vvrouliotis@gmail.com>
2019-01-04 13:32:46 +0200

    Refreshing data live.

--- Assets/Scripts/Interactions/ShowUnitInfo.cs
+++ Assets/Scripts/Interactions/ShowUnitInfo.cs
@@ -8,9 +8,17 @@ namespace Interactions
         public string Name;
         public float MaxHealth, CurrentHealth;
         public Sprite ProfilePic;
-        
+
+        private bool _showData = false;
+
         public override void Select()
         {
+            _showData = true;
+        }
+        
+        void Update()
+        {
+            if (!_showData) return;
             InfoManager.Current.SetPic(ProfilePic);
             InfoManager.Current.SetLines(Name, CurrentHealth + "/" + MaxHealth, "Owner " + GetComponent<Player>().Info.Name);
         }
@@ -19,6 +27,7 @@ namespace Interactions
         {
             InfoManager.Current.ClearPic();
             InfoManager.Current.ClearLines();
+            _showData = false;
         }
     }
 }
