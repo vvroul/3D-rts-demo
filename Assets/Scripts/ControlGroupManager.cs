4a969d7ad2a4f0f09a47c0a3177d17fb77247f48
Vasileios Vrouliotis <vvrouliotis@gmail.com>
2019-01-30 23:30:16 +0200

    Going for health bars and control groups.

---
+++ Assets/Scripts/ControlGroupManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Definitions;
+using UnityEngine;
+using UnityEngine.Serialization;
+using RTS_Camera;
+
+// ReSharper disable Unity.InefficientCameraMainUsage
+
+
+public class ControlGroupManager : MonoBehaviour
+{
+}
